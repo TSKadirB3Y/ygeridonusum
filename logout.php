a12ff7bc10013bd70b4c994e2eddef962608bebc
TSKadirB3Y <43919418+TSKadirB3Y@users.noreply.github.com>
2024-12-02 00:32:16 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Oturumu başlat
-session_unset(); // Tüm oturum değişkenlerini temizle
-session_destroy(); // Oturumu sonlandır
-
-// Kullanıcıyı login sayfasına yönlendir
-header("Location: login.php");
-exit;
-?>
